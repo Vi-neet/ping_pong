d6daeb3ab6b892a914d4e62f6490d37758738ca9
Vi-neet <bhasinvineet49@gmail.com>
2023-07-15 19:16:11 +0530

    Initial commit

---
+++ GameFrame.java
@@ -0,0 +1,22 @@
+import java.awt.Color;
+
+import javax.swing.JFrame;
+
+
+public class GameFrame extends JFrame {
+
+  GamePanel panel;
+
+  GameFrame(){
+    panel = new GamePanel();
+    this.add(panel);
+    this.setTitle("Pong game for loners");
+    this.setResizable(false);
+    this.setBackground(Color.black);
+    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    this.pack();
+    this.setVisible(true);
+    this.setLocationRelativeTo(null);
+  }
+  
+}
